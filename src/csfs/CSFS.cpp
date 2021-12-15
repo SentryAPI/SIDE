a18fbdf5341e5a1ecc81d3d12baf006b329d0dd3
Mike <mallisiscool@gmail.com>
2021-12-15 14:22:10 +0000

    Add blank definitions

--- src/csfs/CSFS.cpp
+++ src/csfs/CSFS.cpp
@@ -4,7 +4,7 @@ namespace CSFS {
     //Returns the current directory (based on system dir)
     //Likely to be ROOT on WASM if the current dir isn't changed!
     std::string GetCurrentDir() {
-
+        return "";
     }
     //Set the current directory
     void SetCurrentDir(std::string dir) {
@@ -12,22 +12,22 @@ namespace CSFS {
     }
     //Get the directory that we started execution in
     std::string GetExecDir() {
-
+        return "";
     }
     //Get all files in current directory
     std::vector<std::string> GetAllFiles() {
-
+        return std::vector<std::string>();
     }
     //Get all files in specified directory (relative to GetCurrentDir)
     std::vector<std::string> GetAllFiles(std::string root) {
-
+        return std::vector<std::string>();
     }
     //Get all directories in current directory
     std::vector<std::string> GetAllDirectories() {
-
+        return std::vector<std::string>();
     }
     //Get all directories in specified directory (relative to GetCurrentDir)
     std::vector<std::string> GetAllDirectories(std::string root) {
-        
+        return std::vector<std::string>();
     }
 };
