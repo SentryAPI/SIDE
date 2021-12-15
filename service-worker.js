3289ee6ece5f070ccd3f19452c6a9f126003a0a0
Mike <mallisiscool@gmail.com>
2021-12-15 22:14:42 +0000

    better service worker from template

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,71 @@
-filesToCache = [
-    "/",
-    "/side.wasm",
-    "/side.js",
-    "/index.html",
-    "/index.css"
+var VERSION = 'side-cache-v1';
+
+var cacheFirstFiles = [
+  'assets/logo.ico'
 ];
-var cache_id = "side-cache-v1";
+
+var networkFirstFiles = [
+  'side.js',
+  'side.wasm',
+  'index.html',
+  'index.css'
+];
+
+// Below is the service worker code.
+
+var cacheFiles = cacheFirstFiles.concat(networkFirstFiles);
 
 self.addEventListener('install', event => {
-    console.log('Installing SIDE...');
-  
-    // cache necessary files
-    event.waitUntil(
-        caches.open(cache_id).then(function (cache) {
-            cache.addAll(filesToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(VERSION).then(cache => {
+      return cache.addAll(cacheFiles);
+    })
+  );
 });
-  
-self.addEventListener('activate', event => {
-    console.log('SIDE is ready to handle fetches!');
-});
-  
+
 self.addEventListener('fetch', event => {
-    event.respondWith(caches.open(cache_id).then(function(cache) {
-        cache.match(event.request).then(function(response){
-            return response || fetch(event.request);
-        })
-    }));
+  if (event.request.method !== 'GET') { return; }
+  if (networkFirstFiles.indexOf(event.request.url) !== -1) {
+    event.respondWith(networkElseCache(event));
+  } else if (cacheFirstFiles.indexOf(event.request.url) !== -1) {
+    event.respondWith(cacheElseNetwork(event));
+  } else {
+    event.respondWith(fetch(event.request));
+  }
 });
+
+// If cache else network.
+// For images and assets that are not critical to be fully up-to-date.
+// developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook/
+// #cache-falling-back-to-network
+function cacheElseNetwork (event) {
+  return caches.match(event.request).then(response => {
+    function fetchAndCache () {
+       return fetch(event.request).then(response => {
+        // Update cache.
+        caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+        return response;
+      });
+    }
+
+    // If not exist in cache, fetch.
+    if (!response) { return fetchAndCache(); }
+
+    // If exists in cache, return from cache while updating cache in background.
+    fetchAndCache();
+    return response;
+  });
+}
+
+// If network else cache.
+// For assets we prefer to be up-to-date (i.e., JavaScript file).
+function networkElseCache (event) {
+  return caches.match(event.request).then(match => {
+    if (!match) { return fetch(event.request); }
+    return fetch(event.request).then(response => {
+      // Update cache.
+      caches.open(VERSION).then(cache => cache.put(event.request, response.clone()));
+      return response;
+    }) || response;
+  });
+}
