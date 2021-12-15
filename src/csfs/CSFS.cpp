758dd15890996e48700c798747b873e23dab2c65
Mike <mallisiscool@gmail.com>
2021-12-15 19:03:24 +0000

    prep for platform specific code

--- src/csfs/CSFS.cpp
+++ src/csfs/CSFS.cpp
@@ -1,14 +1,28 @@
 #include "CSFS.h"
+#include "../platform.h"
 
 namespace CSFS {
+    #if PLATFORM == "WIN"
+    //Doesnt support multiple drives, no real way to achieve this on windows
+    std::string root = "C:/"
+    #else
+    std::string root = "/";
+    #endif
+
+    std::string executing_directory = ""; //TODO: Get the current dir
+    std::string current_directory = executing_directory;
+
     //Returns the current directory (based on system dir)
     //Likely to be ROOT on WASM if the current dir isn't changed!
     std::string GetCurrentDir() {
-        return "";
+        return current_directory;
     }
     //Set the current directory
     void SetCurrentDir(std::string dir) {
-
+        if(dir == "..") {
+            std::string parent_directory = ""; //TODO: String shit to get parent dir
+            current_directory = parent_directory;
+        }
     }
     //Get the directory that we started execution in
     std::string GetExecDir() {
---
+++ src/platform.h
@@ -0,0 +1,16 @@
+#ifndef SRC_PLATFORM
+#define SRC_PLATFORM
+
+#ifdef __EMSCRIPTEN__
+    #define PLATFORM "WASM"
+#elif _WIN32 // note the underscore: without it, it's not msdn official!
+    #define PLATFORM "WIN"
+#elif __unix__ // all unices, not all compilers
+    #define PLATFORM "UNIX"
+#elif __linux__
+    #define PLATFORM "LINUX"
+#elif __APPLE__
+    #define PLATFORM "DAR"
+#endif
+
+#endif /* SRC_PLATFORM */
